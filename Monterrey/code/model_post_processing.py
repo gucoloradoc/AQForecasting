35a50f234ca38d627d5289fdf058f233ee2b49ef
gucoloradoc <zurjak88@gmail.com>
2019-06-27 12:00:52 -0500

    Output renormalization

---
+++ Monterrey/code/model_post_processing.py
@@ -0,0 +1,15 @@
+def data_reescaling(data_scaled, p, norm_param, norm_guide):
+    """ Scales the pollutant according with the norm guide, retrieving the parameters from the
+    norm_param and norm_guide dictionaries.
+    """
+
+    if norm_guide[p][0]=='log':
+        data_rescld=np.exp(data_scaled)-norm_guide[p][1]
+    elif norm_guide[p][0]=='mean':
+        data_rescld=data_scaled*norm_param[p][1]+norm_param[p][0]
+    elif norm_guide[p][0]=='max':
+        data_rescld=data_scaled*(norm_param[p][1]-norm_param[p][0])+norm_param[p][0]
+    elif norm_guide[p][0]=='none':
+        data_rescld=data_scaled-norm_guide[p][1]
+    
+    return data_rescld
